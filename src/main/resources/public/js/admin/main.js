c10dfe5ce5c5f1e542d311d61c22acbf843e05de
nswish <nswish@gmail.com>
2017-01-28 22:09:30 +0800

    admin: 实现侧边栏的选中效果

--- src/main/resources/public/js/admin/main.js
+++ src/main/resources/public/js/admin/main.js
@@ -9,4 +9,16 @@ $(document).ready(function(){
         var $form = $("<form class='hidden' action='/admin/logout' method='post'></form>");
         $form.appendTo(document.body).submit();
     });
+
+    // 分类栏目 折叠/展开
+    $(".panel-heading").click(function(){
+        var $this = $(this);
+
+        $(".panel-heading").parent().addClass("panel-default").removeClass("panel-primary");
+        $(".panel-body").addClass("panel-body-hidden");
+
+        $this.parent().removeClass("panel-default").addClass("panel-primary");
+        var $panelBody = $this.parent().find(".panel-body");
+        $panelBody.removeClass("panel-body-hidden");
+    });
 });
