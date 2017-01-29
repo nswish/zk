92adb7357b59e515907b5a80bf0a7addfdb64d99
nswish <nswish@gmail.com>
2017-01-29 23:55:13 +0800

    admin: 引入underscore + 优化菜单选择和主内容区的标题栏

--- src/main/resources/public/js/admin/main.js
+++ src/main/resources/public/js/admin/main.js
@@ -42,20 +42,58 @@
         $.notify.apply(null, arguments);
     }
 
+    // 选择分类
+    function categorySelected() {
+        var $this = $(this);
+        var url = $this.data('url');
+
+        $("*[data-selected]").removeAttr("data-selected");
+        $this.attr("data-selected", true);
+
+        setMainContentUrl(url);
+    }
+
+    // 设定主内容区的标题
+    function setMainContentTitle() {
+        var $leftSide = $(".left-side");
+        var $selectedCategory = $leftSide.find("*[data-selected=true]");
+
+        var secondCategoryTitle = $selectedCategory.attr('title');
+        var firstCategoryTitle = $selectedCategory.parents(".panel").find(".panel-heading").attr('title');
+
+        var contentDocument = this.contentDocument || this.Document;
+        var thirdCategoryTitle = $(contentDocument).find('head title').text();
+
+        $(".main-content-title").text(_.compact([firstCategoryTitle, secondCategoryTitle, thirdCategoryTitle]).join(" > "));
+    }
+
+    // 设定主内容区载入的页面
+    function setMainContentUrl(url) {
+        var $iframe = $("iframe");
+        $iframe.attr('src', url);
+    }
+
     // 绑定事件
     function bindEvent() {
+        var $leftSide = $(".left-side");
+
         // 登出
         $("#logout_action").click(logout);
 
         // 分类栏目 折叠/展开
-        $(".panel-heading").click(categoryFolding);
+        $leftSide.on('click', '.panel-heading', categoryFolding);
 
         // iframe高度设定
-        $("iframe").on("load", iframeAutoHeight);
+        $("iframe").on("load", iframeAutoHeight).on("load", setMainContentTitle);
+
+        // 选择分类
+        $leftSide.on('click', '.category li', categorySelected);
     }
 
     bindEvent();
 
     // 输出$notify到全局作用域,可供iframe子页面调用
-    window.$notify = notify;
+    _.extend(window, {
+        "$notify": notify
+    });
 })(jQuery);
